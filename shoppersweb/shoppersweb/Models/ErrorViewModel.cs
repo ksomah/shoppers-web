7d19ac5b1f4160b28edcff5ec28fa0eaabd668f1
Kloteh Somah <klsomah@microsoft.com>
2020-09-01 16:45:58 -0400

    Initial commit

---
+++ shoppersweb/shoppersweb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shoppersweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
