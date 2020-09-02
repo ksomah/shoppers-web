ed7ceee44b0a674bd02a3d670592e52d13dd04bc
Kloteh Somah <klsomah@microsoft.com>
2020-09-02 08:55:03 -0400

    fourth commit

--- shoppersweb/shoppersweb/Startup.cs
+++ shoppersweb/shoppersweb/Startup.cs
@@ -13,9 +13,14 @@ namespace shoppersweb
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IHostEnvironment env)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
